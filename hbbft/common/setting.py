a69146e2055e68e8da9a0bb37d36676c7dac2b66
yxiong <yxiong@akamai.com>
2022-05-14 04:02:41 -0400

    1. adjust the project directories framework. 2. accounts and transactions generator. 3. implement user service client and server. 4. make change to the proto files.

---
+++ hbbft/common/setting.py
@@ -0,0 +1,1 @@
+user_service_port=50051
---
+++ hbbft/server/UserServiceHandler/user_service_server.py
@@ -0,0 +1,53 @@
+from concurrent import futures
+import grpc
+from queue import Queue
+from hbbft.common.grpc_tool.generated import user_service_pb2, user_service_pb2_grpc
+from hbbft.common.grpc_tool.generated import hbbft_service_pb2, hbbft_service_pb2_grpc
+
+database = Queue()
+
+
+class UserService(user_service_pb2_grpc.UserServiceServicer):
+    def PayToCall(self, request, context):
+        print("PayTo service requested")
+        try:
+            txn = user_service_pb2.PayToRequest(
+                src_acct=request.src_acct,
+                des_acct=request.des_acct,
+                amount=request.amount,
+                timestamp=request.timestamp
+            )
+            database.put(txn)
+
+        except grpc.RpcError as e:
+            print(e)
+            return user_service_pb2.PayToResponse(status=False)
+        else:
+            return user_service_pb2.PayToResponse(status=True)
+
+
+class HBBFTService(hbbft_service_pb2_grpc.HBBFTServiceServicer):
+    """Missing associated documentation comment in .proto file."""
+
+    def GetTransactions(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        while not database.empty():
+            txn = database.get()
+            yield txn
+
+
+class UserServiceServer(object):
+    def __init__(self, port: int = 50051):
+        self.port = port
+
+    def run(self):
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        user_service_pb2_grpc.add_UserServiceServicer_to_server(UserService(), server)
+        hbbft_service_pb2_grpc.add_HBBFTServiceServicer_to_server(HBBFTService(), server)
+        server.add_insecure_port(f"[::]:{self.port}")
+        server.start()
+        print("Service started")
+        server.wait_for_termination()
+
+
+
