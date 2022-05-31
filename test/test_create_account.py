d2679f01b14c08e080e6b5e12ffec8c5f5185393
yxiong <yxiong@akamai.com>
2022-05-30 22:21:53 -0400

    add test for account create and get balance.

--- test/test_create_account.py
+++ test/test_create_account.py
@@ -1,20 +1,25 @@
+import time
 from hbbft.client.user_service_client import UserServiceClient
 
 
 def test_create_account():
+    time.sleep(10)
     client = UserServiceClient(ip="172.16.238.2", num=2)
-    client.close()
     assert len(client.accts) == 2
-    assert client.accts[0].balance == client.get_balance(client.accts[0].account_id)
+    acct = client.get_balance(client.accts[0].account_id)
+    assert client.accts[0].balance == acct.balance
+    client.close()
 
 
 def test_get_balance():
+    time.sleep(10)
     client = UserServiceClient(ip="172.16.238.2", num=2)
     client.create_txns(1)
+    acct0 = client.get_balance(client.accts[0].account_id)
+    acct1 = client.get_balance(client.accts[1].account_id)
+    while acct0.balance == int(1e9) or acct1.balance == int(1e9):
+        time.sleep(5)
+        acct0 = client.get_balance(client.accts[0].account_id)
+        acct1 = client.get_balance(client.accts[1].account_id)
+    assert abs(client.accts[0].balance - client.accts[1].balance) == abs(acct0.balance - acct1.balance)
     client.close()
-    assert abs(client.accts[0].balance - client.accts[1].balance) == abs(client.get_balance(client.accts[0].account_id) - client.get_balance(client.accts[1].account_id))
-
-
-
-
-
